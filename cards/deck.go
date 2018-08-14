50042da0b13263a15579e3409a00bdbc8f32f285
Shawn Song <mr.song.shawn@gmail.com>
2018-08-14 04:38:46 -0700

    write to file

---
+++ cards/deck.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+// Create a new type of deck which is a slice of string
+
+type deck []string
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Dimands", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+		"Jack", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join(d, ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
---
+++ cards/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := newDeck()
+
+	fmt.Println(cards.toString())
+
+	cards.saveToFile("/tmp/cards")
+}
