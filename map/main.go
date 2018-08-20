866dd55bbf4d3fff30ca5b53218eff57df48a0b7
Shawn Song <mr.song.shawn@gmail.com>
2018-08-20 21:32:01 +1000

    interfaces

--- map/main.go
+++ map/main.go
@@ -5,11 +5,22 @@ import (
 )
 
 func main() {
+
+	// var colors map[string]string
+	// colors := make(map[int]string)
+
 	colors := map[string]string{
-		"red": "#ff0000",
+		"red":   "#ff0000",
+		"blue":  "#00ff00",
+		"white": "#ffffff",
 	}
 
-	colors["blue"] = "#00ff00"
+	printMap(colors)
 
-	fmt.Println(colors)
+}
+
+func printMap(m map[string]string) {
+	for color, hex := range m {
+		fmt.Println(color, hex)
+	}
 }
