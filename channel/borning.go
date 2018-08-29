de6437fba1ccf9408d788a9ea29f68224a3df33d
Shawn Song <mr.song.shawn@gmail.com>
2018-08-29 22:18:40 +1000

    channel

--- channel/borning.go
+++ channel/borning.go
@@ -7,10 +7,34 @@ import (
 )
 
 func borningMain() {
-	c := fanIn(borning("joe"), borning("ann"))
-	for i := 0; i < 10; i++ {
-		fmt.Println(<-c)
+	rand.Seed(time.Now().UnixNano())
+
+	// c1 := fanIn(borning("joe"), borning("ann"))
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(<-c1)
+	// }
+
+	c2 := make(chan string)
+
+	go timeOutChannel(2, c2)
+	sleepTime := rand.Intn(5)
+	fmt.Printf("I'm going to sleep for %d seconds \n", sleepTime)
+	time.Sleep(time.Duration(sleepTime) * time.Second)
+
+	c2 <- "I'm awake now"
+	close(c2)
+	time.Sleep(1 * time.Second)
+}
+
+func timeOutChannel(timeout int, c <-chan string) {
+	select {
+	case msg := <-c:
+		fmt.Println(msg)
+	case <-time.After(time.Duration(timeout) * time.Second):
+		fmt.Println("Timeout. No message received.")
 	}
+
+	fmt.Println(<-c + " is received but discarded")
 }
 
 /**
@@ -29,19 +53,21 @@ func borning1() {
 		fmt.Println(<-ann)
 	}
 	fmt.Println("You are borning. I'm leaving")
-
 }
 
 func fanIn(c1 <-chan string, c2 <-chan string) <-chan string {
 	c := make(chan string)
+	// go func() {for { c <- <-c1}}()
+	// go func() {for {c <- <-c2}}()
+
 	go func() {
 		for {
-			c <- <-c1
-		}
-	}()
-	go func() {
-		for {
-			c <- <-c2
+			select {
+			case s := <-c1:
+				c <- s
+			case s := <-c2:
+				c <- s
+			}
 		}
 	}()
 
